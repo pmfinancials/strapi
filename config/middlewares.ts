b983a5cf8a7c90bfdd1cf9921e6673fbdd7747be
pmfinancials <pmfinancials.dxb@gmail.com>
2026-02-16 23:03:15 +0400

    Change in the middleware and en

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,14 +1,24 @@
 import type { Core } from '@strapi/strapi';
 
-const config: Core.Config.Middlewares = [
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Middlewares => [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:1337', 'https://sthirix.vercel.app'],
+      origin: env.array('CORS_ORIGINS', [
+        'http://localhost:1337',
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'https://sthirix.vercel.app',
+        'https://cms.sthirix.com',
+        'https://sthirix.com',
+        'https://www.sthirix.com',
+      ]),
       credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
     },
   },
   'strapi::poweredBy',
